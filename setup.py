6a54ca67dc174f103cb36517a289523b6094ef38
Yoann Aubineau <yoann@meilleursagents.com>
2010-11-26 19:02:53 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import datetime
+import os
+
+
+def main():
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    configdir = os.path.abspath(os.path.dirname(__file__))
+    homedir = os.path.expanduser('~')
+    for line in open(os.path.join(configdir, 'MANIFEST')):
+        filename = line.strip()
+        source = os.path.join(homedir, filename)
+        target = os.path.join(configdir, filename)
+        if os.path.islink(source):
+            os.unlink(source)
+        if os.path.exists(source):
+            os.rename(source, '.'.join([source, timestamp]))
+        print source + ' -> ' + target
+        os.symlink(target, source)
+
+
+if __name__ == '__main__':
+    main()
+
