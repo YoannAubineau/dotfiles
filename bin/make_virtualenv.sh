e450e91b9ceb5296ccacd0109cf3141ae0da884b
Yoann Aubineau <yoann@meilleursagents.com>
2011-08-04 10:46:13 +0200

    new script make_virtualenv.sh

---
+++ bin/make_virtualenv.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Create Python virtualenv
+# usage: make_virtualenv.sh <version> <virtualenv_name>
+
+set -e
+set -x
+
+
+# Create virtualenvs root directory
+
+if [ "$WORKON_HOME" == "" ]
+then
+    WORKON_HOME=~/.virtualenvs
+fi
+
+mkdir $WORKON_HOME
+
+
+# Create new virtualenv with specified name and Python version
+
+VERSION=$1
+PYTHON=/opt/python-$VERSION/bin/python${VERSION::3}
+VIRTUALENV=/opt/python-$VERSION/bin/virtualenv
+
+VIRTUALENV_NAME=$2
+
+$VIRTUALENV --python=$PYTHON --no-site-packages $WORKON_HOME/$VIRTUALENV_NAME
+
