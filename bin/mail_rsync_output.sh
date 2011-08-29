d892df8d8e4c1563e3e695e84707aa6f787358dc
Yoann Aubineau <yoann@meilleursagents.com>
2011-08-29 11:37:46 +0200

    enhance mail_rsync_output.sh

--- bin/mail_rsync_output.sh
+++ bin/mail_rsync_output.sh
@@ -3,21 +3,36 @@
 # Mail command, customized for rsync output.
 #
 # The message body only contains the stat report, while the whole rsync output
-# is packed into a single text file attached to the mail.
+# is packed into a single zipped text file attached to the mail.
+#
+# The subject of the mail is maid from the second line of rsync output which is
+# the root directory. The sender address is "Backup $HOSTNAME <noreply>". These
+# values can be overritten with command line arguments.
 #
 # Use it as a drop-in replacement to the generic mail command.
-# eg. rsync [...] | mail_rsync_output.sh -s <subject> <email>
+# eg. rsync --verbose [...] | mail_rsync_output.sh -s <subject> <email>
 
 
-TMPDIR=$(mktemp -d)
-ATTACHMENT=$TMPDIR/rsync.$(date +%Y%M%d_%H%M%S).txt
+SENDER="Backup $HOSTNAME <noreply>"
 
+
+TMPDIR=$(mktemp --directory)
+BODY=$TMPDIR/rsync.$(date +%Y%M%d_%H%M%S).txt
 while read LINE
 do
-    echo $LINE >> $ATTACHMENT
+    echo $LINE >> $BODY
 done
 
-PATTERN='/^$/H;x;/\n/{:a;n;p;ba;}'  # drop lines until first blank line
+ATTACHMENT=$BODY
+MAX_FILESIZE=$((25 * 1024 * 1024))  # max attachment size in Gmail: 25MB
+FILESIZE=$(stat --format='%s' $ATTACHMENT)
+if [ $FILESIZE -gt $MAX_FILESIZE ]
+then
+    ATTACHMENT=$BODY.zip
+    zip --quiet $ATTACHMENT $BODY
+fi
 
-cat $ATTACHMENT | sed -n $PATTERN | mail -a $ATTACHMENT $@
-rm -rf $TMPDIR
+PATTERN='/^$/H;x;/\n/{:a;n;p;ba;}'  # drop lines until first blank line
+sed --quiet $PATTERN $BODY | mail -r "$SENDER" -a "$ATTACHMENT" $@
+
+rm --recursive --force $TMPDIR
