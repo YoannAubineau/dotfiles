d2c416dcbfccb9a03999f69a350bf913f2938eb3
Yoann Aubineau <yoann@meilleursagents.com>
2012-11-12 01:42:52 +0100

    new script set_permissions_for_shared_directory.sh

---
+++ bin/set_permissions_for_shared_directory.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Usage: set_permissions_for_shared_directory.sh <group> <target>
+
+set -x -e
+
+GROUP=$1
+TARGET=$2
+
+# Fix permissions
+chgrp -R $GROUP $TARGET
+chmod -R ug=rwX,o=X $TARGET
+find $TARGET -type d -exec chmod g+s {} \;
+
+# Force more permissive umask
+setfacl -Rm d:g::rwx,d:o::--- $TARGET
+
