b1271e59c4a5b6dabeebdd5569d9e928ac81dd57
Yoann Aubineau <yoann@meilleursagents.com>
2014-02-11 11:57:50 +0100

    remove copy_table.sh script

--- bin/copy_table.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TABLENAME=$1
-
-pg_dump -h sql1.meilleursagents -U meilleursagents meilleursagents -t $TABLENAME -F custom > $TABLENAME.dump
-pg_restore -h localhost -U meilleursagents_dev -d meilleursagents_dev --clean --jobs=8 $TABLENAME.dump
-
-rm $TABLENAME.dump
-
