e8fb0f161e46b8cfd7cd19aad539b9c17833888a
Yoann Aubineau <y.aubineau@criteo.com>
2014-10-17 15:06:34 +0200

    better make_virtualenv.sh

--- bin/make_virtualenv.sh
+++ bin/make_virtualenv.sh
@@ -7,29 +7,32 @@ set -e -x
 VERSION=$1
 VIRTUALENV_NAME=$2
 
+VERSION_MAJOR=${VERSION::1}
+VERSION_MINOR=${VERSION::3}
+
 
 # Create virtualenvs root directory
 
-if [ "$WORKON_HOME" == "" ]
+if [[ $WORKON_HOME == "" ]]
 then
     WORKON_HOME=~/.virtualenvs
 fi
 
 mkdir -p $WORKON_HOME
+VIRTUALENV_PATH=$WORKON_HOME/$VIRTUALENV_NAME
 
 
 # Create new virtualenv with specified name and Python version
 
-[ -d "/System" ] && PREFIX="/System" || PREFIX="/opt"
-PYTHON="$PREFIX/python-$VERSION/bin/python${VERSION::3}"
-VIRTUALENV="$PREFIX/python-$VERSION/bin/virtualenv"
+[[ -d "/System" ]] && PREFIX="/System" || PREFIX="/opt"
+PYTHON=$PREFIX/python-$VERSION/bin/python$VERSION_MINOR
 
-$VIRTUALENV --python=$PYTHON --no-site-packages $WORKON_HOME/$VIRTUALENV_NAME
-
-
-# Install virtualenvwrapper for this virtualenv so that it doesn't break using tmux
-
-#source $WORKON_HOME/$VIRTUALENV_NAME/bin/activate
-#pip install virtualenvwrapper
-#deactivate
+if [[ $VERSION_MAJOR == "3" ]]
+then
+    VIRTUALENV="$PREFIX/python-$VERSION/bin/pyvenv-$VERSION_MINOR"
+    $VIRTUALENV $VIRTUALENV_PATH
+else
+    VIRTUALENV="$PREFIX/python-$VERSION/bin/virtualenv"
+    $VIRTUALENV --python=$PYTHON --no-site-packages $VIRTUALENV_PATH
+fi
 
