de0ebeb4a4be3444c3e25fec2b6d9e09d710c623
Yoann Aubineau <y.aubineau@criteo.com>
2015-09-17 11:15:16 +0200

    java: set JAVA_HOME depending on the setup

--- .bashrc.d/90.java
+++ .bashrc.d/90.java
@@ -1,6 +1,12 @@
 # Java-specific configuration
 
-export JAVA_HOME=/usr/lib/jvm/default-java
+if [[ -d /usr/lib/jvm/java ]]; then
+    export JAVA_HOME=/usr/lib/jvm/java
+fi
+
+if [[ -d /usr/lib/jvm/default-java ]]; then
+    export JAVA_HOME=/usr/lib/jvm/default-java
+fi
 
 # This is a ugly hack to ensure that applications relying on JAVA_HOME
 # environment variable are transparently kept in sync with the java version
