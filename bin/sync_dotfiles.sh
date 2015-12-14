75602c0c099c27902c776ad20cd116cee2c9c2e4
Yoann Aubineau <y.aubineau@criteo.com>
2015-12-14 10:16:13 +0100

    add sync_dotfiles.sh

---
+++ bin/sync_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+    TARGETS=$(grep -e '--sync-dotfiles' .ssh/config | sed 's/\s+/\s/g' | cut -d' ' -f2)
+else
+    TARGETS="$@"
+fi
+
+for TARGET in ${TARGETS}; do
+    echo "-------------------------------------------------------------------------------"
+    echo "Syncing dotfiles to ${TARGET^^}…"
+
+    CMD="rsync -avz ~/.dotfiles ${TARGET}: --delete | grep -v '^$' && ssh ${TARGET} .dotfiles/install.py"
+    [[ $ASYNC -eq 1 ]] && CMD="(($CMD) 2>&1 | sed 's/^/[$TARGET] /')&"
+    eval $CMD
+done
+
