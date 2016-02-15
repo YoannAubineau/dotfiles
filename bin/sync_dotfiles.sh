8ce59bab3451b10cd3416c5dff7c4ad7b376f77d
Yoann Aubineau <y.aubineau@criteo.com>
2016-02-15 16:29:45 +0100

    sync_dotfiles: chmod 600 on remote .dotfiles

--- bin/sync_dotfiles.sh
+++ bin/sync_dotfiles.sh
@@ -10,7 +10,7 @@ for TARGET in ${TARGETS}; do
     echo "-------------------------------------------------------------------------------"
     echo "Syncing dotfiles to ${TARGET^^}…"
 
-    CMD="rsync -avz ~/.dotfiles ${TARGET}: --delete | grep -v '^$' && ssh ${TARGET} .dotfiles/install.py"
+    CMD="rsync -avz ~/.dotfiles ${TARGET}: --delete | grep -v '^$' && chmod -R go=--- ~/.dotfiles && ssh ${TARGET} .dotfiles/install.py"
     [[ $ASYNC -eq 1 ]] && CMD="(($CMD) 2>&1 | sed 's/^/[$TARGET] /')&"
     eval $CMD
 done
